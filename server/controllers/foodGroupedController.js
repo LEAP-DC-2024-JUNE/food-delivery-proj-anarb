bf8e6ceceaa9e926aa578dbb1853abd6d5441afb
anarb0 <anarb359@gmail.com>
2025-04-29 21:01:47 -0400

    .

--- server/controllers/foodGroupedController.js
+++ server/controllers/foodGroupedController.js
@@ -2,25 +2,55 @@ import Food from "../models/foodModel.js";
 import FoodOrder from "../models/foodOrderModel.js";
 import FoodCategory from "../models/foodCategoryModel.js";
 
+// export const getFoodsGroupedByCategory = async (req, res) => {
+//   try {
+//     const groupedFoods = await Food.aggregate([
+//       {
+//         $group: {
+//           _id: "$category",
+//           foods: { $push: "$$ROOT" },
+//         },
+//       },
+//     ]);
+//     const result = await FoodCategory.populate(groupedFoods, {
+//       path: "_id",
+//       select: "categoryName",
+//     });
+
+//     res.status(200).json(result);
+//   } catch (error) {
+//     res.status(500).json({
+//       message: error.message,
+//     });
+//   }
+// };
+
 export const getFoodsGroupedByCategory = async (req, res) => {
   try {
-    const groupedFoods = await Food.aggregate([
-      {
-        $group: {
-          _id: "$category",
-          foods: { $push: "$$ROOT" },
+    const allCategories = await FoodCategory.find({}, "_id categoryName");
+    const allFoods = await Food.find({}).populate(
+      "category",
+      "_id categoryName"
+    );
+
+    const grouped = allCategories.map((category) => {
+      const foods = allFoods.filter(
+        (food) =>
+          food.category &&
+          food.category._id.toString() === category._id.toString()
+      );
+      return {
+        _id: {
+          _id: category._id,
+          categoryName: category.categoryName,
         },
-      },
-    ]);
-    const result = await FoodCategory.populate(groupedFoods, {
-      path: "_id",
-      select: "categoryName",
+        foods,
+      };
     });
-    res.status(200).json(result);
+
+    res.status(200).json(grouped);
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 };
 
